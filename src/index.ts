c4dddcf30cd00f8f5471294f5045222cc6668824
Rory& <root@rory.gay>
2025-01-31 16:46:12 +0100

    Mostly working sample

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@ import { XAPI, connect } from "jsxapi";
 import { run } from "./macro";
 require("dotenv").config();
 
+const clientAuth = {
+  "user_id": process.env.ROOMKIT_MATRIX_USER_ID,
+  "device_id": process.env.ROOMKIT_MATRIX_DEVICE_ID,
+  "access_token": process.env.ROOMKIT_MATRIX_ACCESS_TOKEN,
+  "passwordlessUser": process.env.ROOMKIT_MATRIX_PASSWORDLESS_USER,
+  "tempPassword": process.env.ROOMKIT_MATRIX_TEMP_PASSWORD
+};
+
 async function init() {
   const xapi = connect(process.env.ROOMKIT_URL!, {
     username: process.env.ROOMKIT_USER!,
--- src/macro.ts
+++ src/macro.ts
@@ -1,68 +1,128 @@
-import { XAPI } from "jsxapi";
+import {XAPI} from "jsxapi";
+import {log} from "node:console";
 
 // FOR MACRO: import xapi from 'xapi';
 export async function run(deviceApi: XAPI) {
-  const meetingDomain = "call.element.io";
+    //#region Configuration
+    // const elementCallDomain = "call.element.io";
+    const elementCallDomain = "ec.rory.gay";
+    const displayName = "Room Kit Mini";
 
-  const KEYBOARD_TYPES = {
-    NUMERIC: "Numeric",
-    SINGLELINE: "SingleLine",
-    PASSWORD: "Password",
-    PIN: "PIN",
-  };
+    // Obtain from client devtools -> Storage -> Local Storage -> "matrix-auth-store"
+    // const clientAuth = {
+    //     "user_id": ...,
+    //     "device_id": ...,
+    //     "access_token": ...,
+    //     "passwordlessUser": true,
+    //     "tempPassword": ...
+    // };
+    //#endregion
 
-  const MEETING_ID = "meetingID";
+    //#region Constants
+    const KEYBOARD_TYPES = {
+        NUMERIC: "Numeric",
+        SINGLELINE: "SingleLine",
+        PASSWORD: "Password",
+        PIN: "PIN",
+    };
 
-  /* This will be the Panel/Widget ID you are using in the UI Extension */
-  const INROOMCONTROL_AUDIOCONTROL_PANELID = "ElementCallButton";
+    const MEETING_ID = "meetingID";
 
-  /* Use this one if you want to limit calls to numeric only. In this example, require number to be between 3 and 10 digits. */
-  const REGEXP_NUMERICDIALER = /^([0-9]{3,10})$/;
+    /* This will be the Panel/Widget ID you are using in the UI Extension */
+    const INROOMCONTROL_AUDIOCONTROL_PANELID = "ElementCallButton";
 
-  function getMeetingID(text: string, value = "") {
-    deviceApi.Command.UserInterface.Message.TextInput.Display({
-      InputType: KEYBOARD_TYPES.SINGLELINE,
-      Placeholder: `No need to type :${meetingDomain}`,
-      Title: "Join meeting via Element Call",
-      Text: text,
-      InputText: value,
-      SubmitText: "Join",
-      FeedbackId: MEETING_ID,
-    }).catch((error: any) => console.error(error));
-  }
+    /* Use this one if you want to limit calls to numeric only. In this example, require number to be between 3 and 10 digits. */
+    const REGEXP_NUMERICDIALER = /^([0-9]{3,10})$/;
 
-  /* This is the listener for the in-room control panel button that will trigger the dial panel to appear */
-  deviceApi.Event.UserInterface.Extensions.Panel.Clicked.on((event: any) => {
-    if (event.PanelId === INROOMCONTROL_AUDIOCONTROL_PANELID) {
-      getMeetingID("Enter the meeting id from your invite:");
+    //#endregion
+
+    // function getMeetingID(text: string, value = "") {
+    //     deviceApi.Command.UserInterface.Message.TextInput.Display({
+    //         InputType: KEYBOARD_TYPES.SINGLELINE,
+    //         Placeholder: `No need to type :${elementCallDomain}`,
+    //         Title: "Join meeting via Element Call",
+    //         Text: text,
+    //         InputText: value,
+    //         SubmitText: "Join",
+    //         FeedbackId: MEETING_ID,
+    //     }).catch((error: any) => console.error(error));
+    // }
+
+
+    function getText(info: Partial<InputPrompt>): Promise<string> {
+        return new Promise((resolve, reject) => {
+            const feedbackId = Math.random().toString();
+            deviceApi.Command.UserInterface.Message.TextInput.Display(
+                {
+                    InputType: info.InputType ?? KeyboardTypes.SINGLELINE,
+                    Placeholder: info.Placeholder ?? "",
+                    Title: info.Title ?? "",
+                    Text: info.Description ?? "",
+                    InputText: info.DefaultValue ?? "",
+                    SubmitText: info.SubmitText ?? "OK",
+                    FeedbackId: feedbackId
+                }
+            ).catch((error: any) => console.error(error));
+            deviceApi.Event.UserInterface.Message.TextInput.Response.once(async (event: any) => {
+                if (event.FeedbackId == feedbackId) {
+                    resolve(event.Text);
+                } else reject("Got out of order getText response!");
+            });
+        })
     }
-  });
 
-  /* Event listener for the dial pad being posted */
-  deviceApi.Event.UserInterface.Message.TextInput.Response.on((event: any) => {
-    switch (event.FeedbackId) {
-      case MEETING_ID:
-        /* Change this to whatever filter you want to check for validity */
-        /*const regex = REGEXP_NUMERICDIALER;
-                const match = regex.exec(event.Text);
+    /* This is the listener for the in-room control panel button that will trigger the dial panel to appear */
+    deviceApi.Event.UserInterface.Extensions.Panel.Clicked.on(async (event: any) => {
+        if (event.PanelId === INROOMCONTROL_AUDIOCONTROL_PANELID) {
+            const serverDomain = new URL(await getMatrixServerDomain(elementCallDomain)).host;
+            const alias = await getText({
+                Title: "Enter call name",
+                Description: `No need to type :${serverDomain}`,
+                Placeholder: "my-awesome-call",
+            });
+            console.log(alias);
+            const roomId = await getText({
+                Title: "Enter room ID",
+                Description: "... or press Next is a value is already there",
+                DefaultValue: await tryResolveRoomAlias(`#${alias}:${serverDomain}`)
+            });
+            const roomEncryptionEnabled = await checkEncryptionEnabled(roomId);
+            const password = await getText({
+                Title: "Call password/pin",
+                Description: `Call may require password/pin: ${!roomEncryptionEnabled}`,
+                Placeholder: "Call password..."
+            });
 
-                if (match) {
-                    const meetingID = match[1];
-                    const at = meetingDomain.startsWith('@') ? '' : '@';
-                    const Number = meetingID + at + meetingDomain;
-                    console.log('Dial:', Number);
+            let url = `https://${elementCallDomain}/room/#/`
+                + `?displayName=${encodeURIComponent(displayName)}`
+                // + `&appPrompt=false`
+                // + `&skipLobby=true`
+                // + `&returnToLobby=false`
+                // + `&showControls=false`
+                // + `&hideHeader=true`
+                + `&isHeadless=true`;
+            if (password) url += `&password=${encodeURIComponent(password)}`;
+            url += `&roomId=${roomId}`
 
-                    //xapi.Command.Dial({ Number });
-                } else {
-                    getMeetingID("You typed in an invalid number. Please try again.", event.Text);
-                }*/
-        break;
-    }
-  });
+            console.log("Opening page: ", url)
+            await deviceApi.Command.UserInterface.WebView.Display({
+                Url: url,
+            });
+        }
+    });
 
-  // Register button:
-  async function registerHomeButton() {
-    const xml = `<?xml version="1.0"?>
+    /* Event listener for the dial pad being posted */
+    // deviceApi.Event.UserInterface.Message.TextInput.Response.on((event: any) => {
+    //   switch (event.FeedbackId) {
+    //     case MEETING_ID:
+    //
+    //       break;
+    //   }
+    // });
+
+    // Register button:
+    async function registerHomeButton() {
+        const xml = `<?xml version="1.0"?>
   <Extensions>
     <Version>1.7</Version>
     <Panel>
@@ -76,20 +136,95 @@ export async function run(deviceApi: XAPI) {
       <ActivityType>Custom</ActivityType>
     </Panel>
   </Extensions>`;
-    console.log(xml);
+        console.log(xml);
 
-    await deviceApi.Command.UserInterface.Extensions.Panel.Save(
-      {
-        PanelId: INROOMCONTROL_AUDIOCONTROL_PANELID,
-      },
-      xml,
-    );
-  }
+        await deviceApi.Command.UserInterface.Extensions.Panel.Save(
+            {
+                PanelId: INROOMCONTROL_AUDIOCONTROL_PANELID,
+            },
+            xml,
+        );
+    }
 
-  await registerHomeButton();
+
+    //#region Basic HTTP wrappers
+    async function httpGet(uri: string, headers: string[] = []) {
+        log("Sending matrix GET:", uri)
+        const res = await deviceApi.Command.HttpClient.Get({
+            Url: uri,
+            Header: headers,
+            //ResultBody: "PlainText"
+        });
+        return JSON.parse(res.Body);
+    }
+
+    //#endregion
+
+    //#region Matrix API
+    async function getMatrixServerDomain(ecInstance: string) {
+        const ecConfig = await httpGet(`https://${ecInstance}/config.json`);
+        const serverDomain = ecConfig.default_server_config["m.homeserver"].base_url;
+        console.log(`Resolved ${ecInstance}'s server domain to be ${serverDomain}`);
+        return serverDomain;
+    }
+
+    async function createRoomWithAlias(name: string, alias: string) {
+
+    }
+
+    async function tryResolveRoomAlias(alias: string) {
+        const server = await getMatrixServerDomain(elementCallDomain);
+        const encodedAlias = encodeURIComponent(alias);
+        try {
+            // const res = await httpGet("https://matrix.rory.gay/_matrix/client/v3/directory/room/" + encodeURIComponent("#proxmox:envs.net"));
+            const res = await httpGet(`${server}/_matrix/client/v3/directory/room/${encodedAlias}`);
+            log(res);
+            return res.room_id;
+        } catch (e: any) {
+            log(e);
+        }
+    }
+
+    async function checkEncryptionEnabled(roomId: string) {
+        const server = await getMatrixServerDomain(elementCallDomain);
+        const encodedRoomId = encodeURIComponent(roomId);
+        try {
+            const res = await httpGet(`${server}/_matrix/client/v3/room/${encodedRoomId}/state/m.room.encryption/`);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    //#endregion
+
+
+    await registerHomeButton();
+    await tryResolveRoomAlias("#cisco:call.ems.host");
+    await deviceApi.Command.UserInterface.WebView.Clear();
+
 }
 
-async function getMatrixServerDomain() {}
+
+//#region Types
+interface InputPrompt {
+    InputType: KeyboardTypes,
+    Placeholder: string,
+    Title: string, // Title
+    Description: string, // Description
+    DefaultValue: string, // default value
+    SubmitText: string, // OK button
+}
+
+enum KeyboardTypes {
+    NUMERIC = "Numeric",
+    SINGLELINE = "SingleLine",
+    PASSWORD = "Password",
+    PIN = "PIN",
+}
+
+//#endregion
 
 // FOR MACRO: -- NOTE: no top level await!
 //run(xapi).catch()
+
