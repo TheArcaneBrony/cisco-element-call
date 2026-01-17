a1744af7ae9b716510e794969b5780acd161994c
Rory& <root@rory.gay>
2026-01-17 15:26:15 +0100

    Maintenance: Update dependencies, drop hashes.json

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
-  description = "Spacebar server, written in Typescript.";
+  description = "A macro to host element call on a Cisco WebEx device.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -18,61 +18,62 @@
         pkgs = import nixpkgs {
           inherit system;
         };
-        hashesFile = builtins.fromJSON (builtins.readFile ./hashes.json);
         lib = pkgs.lib;
       in
       {
         packages = {
-          default = pkgs.buildNpmPackage {
-            pname = "cisco-element-call";
-            name = "cisco-element-call";
+          default =
+            let
+              filteredSrc = lib.fileset.toSource {
+                root = ./.;
+                fileset = (
+                  lib.fileset.intersection ./. (
+                    lib.fileset.unions [
+                      ./src
+                      ./package.json
+                      ./package-lock.json
+                      ./tsconfig.json
+                    ]
+                  )
+                );
+              };
+            in
+            pkgs.buildNpmPackage {
+              pname = "cisco-element-call";
+              name = "cisco-element-call";
 
-            meta = with lib; {
-              description = "A call to host element call on a Cisco WebEx device.";
-              homepage = "https://github.com/TheArcaneBrony/cisco-element-call";
-              #license = licenses.agpl3Plus;
-              platforms = platforms.all;
-              mainProgram = "start-macro";
+              meta = with lib; {
+                description = "A maco to host element call on a Cisco WebEx device.";
+                homepage = "https://github.com/TheArcaneBrony/cisco-element-call";
+                #license = licenses.agpl3Plus;
+                platforms = platforms.all;
+                mainProgram = "start-macro";
+              };
+
+              src = filteredSrc;
+              nativeBuildInputs = with pkgs; [ python3 ];
+              npmDeps = pkgs.importNpmLock { npmRoot = filteredSrc; };
+              npmConfigHook = pkgs.importNpmLock.npmConfigHook;
+
+              makeCacheWritable = true;
+              postPatch = ''
+                substituteInPlace package.json --replace 'npx patch-package' '${pkgs.nodePackages.patch-package}/bin/patch-package'
+              '';
+              installPhase = ''
+                runHook preInstall
+                set -x
+                #remove packages not needed for production, or at least try to...
+                npm prune --omit dev --no-save $npmInstallFlags "''${npmInstallFlagsArray[@]}" $npmFlags "''${npmFlagsArray[@]}"
+                find node_modules -maxdepth 1 -type d -empty -delete
+
+                mkdir -p $out
+                cp -r dist node_modules package.json $out/
+                makeWrapper ${pkgs.nodejs}/bin/node $out/bin/start-macro --prefix NODE_PATH : $out/node_modules --add-flags $out/dist/index.js
+
+                set +x
+                runHook postInstall
+              '';
             };
-
-            src = ./.;
-            nativeBuildInputs = with pkgs; [ python3 ];
-            npmDepsHash = hashesFile.npmDepsHash;
-            makeCacheWritable = true;
-            postPatch = ''
-              substituteInPlace package.json --replace 'npx patch-package' '${pkgs.nodePackages.patch-package}/bin/patch-package'
-            '';
-            installPhase = ''
-              runHook preInstall
-              set -x
-              #remove packages not needed for production, or at least try to...
-              npm prune --omit dev --no-save $npmInstallFlags "''${npmInstallFlagsArray[@]}" $npmFlags "''${npmFlagsArray[@]}"
-              find node_modules -maxdepth 1 -type d -empty -delete
-
-              mkdir -p $out
-              cp -r dist node_modules package.json $out/
-              makeWrapper ${pkgs.nodejs}/bin/node $out/bin/start-macro --prefix NODE_PATH : $out/node_modules --add-flags $out/dist/index.js
-
-              set +x
-              runHook postInstall
-            '';
-          };
-
-          update-nix = pkgs.writeShellApplication {
-            name = "update-nix";
-            runtimeInputs = with pkgs; [
-              prefetch-npm-deps
-              nix
-              jq
-            ];
-            text = ''
-              nix flake update --extra-experimental-features 'nix-command flakes'
-              DEPS_HASH=$(prefetch-npm-deps package-lock.json)
-              TMPFILE=$(mktemp)
-              jq '.npmDepsHash = "'"$DEPS_HASH"'"' hashes.json > "$TMPFILE"
-              mv -- "$TMPFILE" hashes.json
-            '';
-          };
         };
 
         devShell = pkgs.mkShell {
